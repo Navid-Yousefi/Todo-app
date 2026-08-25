35b1c9a8393e8d7e6d746c2931ec1aa582d2ee40
Navid Yousife <104007176+NavidYosufie@users.noreply.github.com>
2026-08-25 18:26:12 +0330

    fix a syntax

--- app/core/config.py
+++ app/core/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    SQLALCHEMY_DATABASE_URL: str = 'aqlite:///sqlite.db'
+    SQLALCHEMY_DATABASE_URL: str = 'sqlite:///sqlite.db'
     JWT_SECRET_KEY: str = 'test'
     REDIS_URL: str = 'redis://localhost:6379'
     SENTRY_DSN: str = 'test'
